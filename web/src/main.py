cff5cf76b04f569f908278a4fabecbabca684280
Ivan Persidsky <Colt.spb@gmail.com>
2022-01-27 21:44:44 +0300

    specific navs

--- web/src/main.py
+++ web/src/main.py
@@ -3,6 +3,7 @@ import time
 
 from flask import Flask, redirect, render_template, request, abort, Blueprint
 from template_helpers import setup_context_processors
+from functools import wraps
 import file_download, redirs, forms, pages
 
 app = Flask(__name__)
@@ -47,6 +48,38 @@ def post_form():
     return redirect_final("Ok")
 
 
+@app.route("/error-500")
+def error_500():
+    abort(500)
+
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
+
+def check_auth(username, password):
+    return len(username) > 0
+
+def login_required(f):
+    @wraps(f)
+    def wrapped_view(**kwargs):
+        auth = request.authorization
+        if not (auth and check_auth(auth.username, auth.password)):
+            return ('Unauthorized', 401, {
+                'WWW-Authenticate': 'Basic realm="Login Required"'
+            })
+
+        return f(**kwargs)
+
+    return wrapped_view
+
+@app.route('/basic-auth')
+@login_required
+def basic_auth():
+    return render_template('basic-auth.html', auth=request.authorization)
+
+
+@app.route('/basic-auth-logout')
+@login_required
+def basic_auth_logout():
+    return (render_template('401.html'), 401)
